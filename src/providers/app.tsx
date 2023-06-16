714d6160d8286038a9ce56cf13952fb6d27862ee
ao-takeda <ao_21n1103123@nnn.ed.jp>
2023-06-16 22:00:58 +0900

    エラーハンドリング画面実装

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,28 +1,49 @@
-import {CircularProgress, Grid, ThemeProvider} from "@mui/material";
+import {Button, CircularProgress, Grid, ThemeProvider, Typography} from "@mui/material";
 import {theme} from "@/theme.ts";
 import {FC, ReactNode, Suspense} from "react";
 import {QueryClientProvider} from "react-query";
 import {queryClient} from "@/lib/react-query.ts";
+import {ErrorBoundary} from "react-error-boundary";
 
 interface Props {
   children?: ReactNode;
 }
 
 export const AppProvider: FC<Props> = (props) => {
-  // TODO: エラーハンドリング画面実装
   return (
     <ThemeProvider theme={theme}>
       <Suspense
         fallback={
-          <Grid container width="100%" height="100%" alignItems='center' justifyContent='center'>
+          <Grid container width="100%" height="100%" alignItems="center" justifyContent="center">
             <Grid item><CircularProgress/></Grid>
           </Grid>
         }
       >
-        <QueryClientProvider client={queryClient}>
-          {props.children}
-        </QueryClientProvider>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <QueryClientProvider client={queryClient}>
+            {props.children}
+          </QueryClientProvider>
+        </ErrorBoundary>
       </Suspense>
     </ThemeProvider>
   );
 };
+
+const ErrorFallback = () => {
+  return (
+    <Grid container width="100%" height="100%" alignItems="center" justifyContent="center" flexDirection="column">
+      <Grid item mb={2}>
+        <Typography color="#f56565">エラーが発生しました。</Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => window.location.assign(window.location.origin)}
+        >
+          Refresh
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
