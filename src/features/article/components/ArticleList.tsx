7a34f78a4351c8b18a47181faf0357ff781172f9
ao-takeda <ao_21n1103123@nnn.ed.jp>
2023-06-17 16:52:49 +0900

    記事取得中スケルトンが表示されてなかったので修正

--- src/features/article/components/ArticleList.tsx
+++ src/features/article/components/ArticleList.tsx
@@ -1,34 +1,34 @@
-import {ArticleCard} from "./ArticleCard";
-import {FC} from "react";
-import {Box} from "@mui/material";
-import {useAtom} from "jotai";
-import {countryAtom} from "@/features/language";
-import {useArticles} from "@/features/article/api/getArticles.ts";
-import {queryAtom} from "@/features/article";
-import {SkeletonCard} from "@/features/article/components/SkeletonCard.tsx";
-import {NoArticle} from "@/features/article/components/NoArticle.tsx";
+import {ArticleCard} from "./ArticleCard"
+import {FC} from "react"
+import {Box} from "@mui/material"
+import {useAtom} from "jotai"
+import {countryAtom} from "@/features/language"
+import {useArticles} from "@/features/article/api/getArticles.ts"
+import {queryAtom} from "@/features/article"
+import {SkeletonCard} from "@/features/article/components/SkeletonCard.tsx"
+import {NoArticle} from "@/features/article/components/NoArticle.tsx"
 
 export const ArticleList: FC = () => {
-  const [country] = useAtom(countryAtom);
-  const [query] = useAtom(queryAtom);
-  const {data: articles, isLoading} = useArticles({country, query});
+  const [country] = useAtom(countryAtom)
+  const [query] = useAtom(queryAtom)
+  const {data: articles, isLoading} = useArticles({country, query})
 
-  if (!articles?.length) {
-    return (<NoArticle/>);
-  } else {
+  if (articles?.length || isLoading) {
     return (
       <Box px={{xs: 1, lg: 3}}>
         {
-          !isLoading
-            ? articles.map((article) => (
-              <ArticleCard article={article} key={article.url}/>
-            ))
-            : Array.from({length: 10}).map((_, index) => (
+          isLoading
+            ? Array.from({length: 10}).map((_, index) => (
               <SkeletonCard key={index}/>
             ))
+            : articles && articles.map((article) => (
+            <ArticleCard article={article} key={article.url}/>
+          ))
         }
       </Box>
-    );
+    )
+  } else {
+    return (<NoArticle/>)
   }
-};
+}
 
